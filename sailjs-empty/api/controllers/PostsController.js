f388c84e16bcbc763f28ed462b2466fd5d5e7a50
Le Trong Tri <trichuot99@gmail.com>
2019-11-20 14:22:37 +0700

    wednesdayafternoon

--- sailjs-empty/api/controllers/PostsController.js
+++ sailjs-empty/api/controllers/PostsController.js
@@ -4,104 +4,138 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-
+const fs = require('fs');
 module.exports = {
     upload: (req, res) => {
         req.file('picture').upload({
             dirname: require('path').resolve(sails.config.appPath, 'assets/images')
         }, function(err, uploadedFiles) {
-            const urlParts = uploadedFiles[0].fd.split("/");
-            const picName = urlParts[urlParts.length - 1];
-            res.json({
-                url: `http://localhost:1337/images/${picName}`
-            })
+            if (err) {
+                return res.json({
+                    success: false,
+                    message: 'Upload failed'
+                })
+            } else {
+                const urlParts = uploadedFiles[0].fd.split("/");
+                const picName = urlParts[urlParts.length - 1];
+                return res.json({
+                    url: `http://localhost:1337/images/${picName}`
+                })
+            }
         })
     },
     create: (req, res) => {
+        var author;
         Posts.create({
             title: req.body.title,
             picture: req.body.picture,
             description: req.body.description,
             author: req.session.currentUser.id
-        }).fetch().exec((error, data) => {
+        }).fetch().then((data) => {
+
+            author = data.author;
             var ts = new Date();
-            var time = ts.toLocaleString("vi-VN", { timeZone: "Asia/Bangkok" })
-            Posts.update({ id: data.id }, {
-                makeday: time
-            }).fetch().exec((error, data) => {})
-            User.findOne({ id: data.author }).exec((error2, data2) => {
-                up = ++data2.post;
-                User.update({ id: data.author }, { post: up }).fetch().exec((error3, data3) => {})
+            var time = ts.toLocaleString("vi-VN", { timeZone: "Asia/Bangkok" });
+            return Posts.update({ id: data.id }, { makeday: time }).fetch();
+        }).then((data2) => {
+            return User.findOne({ id: author })
+        }).then((data3) => {
+            var up = ++data3.post;
+            return User.update({ id: author }, { post: up }).fetch();
+
+        }).then((data4) => {
+            return res.json({
+                success: true,
+                message: 'Create Post Success'
             })
-            if (error) {
-                res.json({
-                    success: false,
-                    message: 'Create Post Failed'
-                })
-            } else {
-                res.json({
-                    success: true,
-                    message: 'Create Post Success'
-                })
-            }
+        }).catch((e) => {
+            return res.json({
+                success: false,
+                message: 'Create Post Failed'
+            })
         })
     },
     show: (req, res) => {
         var pageNumber = Number(req.query.pageNumber);
         var total;
-        Posts.count().exec((error, data) => {
+        Posts.count().then((data) => {
             total = data;
-        });
-        if (!req.query.pageNumber) {
-            Posts.find({ limit: 5, skip: 0 }).populate('author').sort([{ createdAt: 'desc' }]).exec((error, data) => {
-                return res.view('pages/index', { post: data, total: total, page: 1 })
-            })
-        } else {
-            Posts.find({ limit: 5, skip: 5 * (pageNumber - 1) }).populate('author').sort([{ createdAt: 'desc' }]).exec((error, data) => {
-                return res.view('pages/index', { post: data, total: total, page: pageNumber })
-            })
-        }
+            if (!req.query.pageNumber) {
+                return Posts.find({ limit: 5, skip: 0 }).populate('author').sort([{ createdAt: 'desc' }])
+            } else {
+                return Posts.find({ limit: 5, skip: 5 * (pageNumber - 1) }).populate('author').sort([{ createdAt: 'desc' }])
+            }
+
+        }).then((data2) => {
+            return res.view('pages/index', { post: data2, total: total, page: req.query.pageNumber ? pageNumber : 1 })
+        }).catch((e) => {
+            req.session.flash = {
+                message: e
+            }
+            return res.redirect('/error');
+        })
 
     },
     showInfo: (req, res) => {
-
         return res.view('pages/articlemanager')
 
     },
     detail: (req, res) => {
         Posts.findOne({ id: req.params.id }).populate('author').exec((error, data) => {
-            if (!data) {
+            if (error) {
+                req.session.flash = {
+                    message: error
+                }
+                return res.redirect('/error');
+            } else if (!data) {
                 req.session.flash = {
                     message: "No post found"
                 }
-                res.redirect('/error');
+                return res.redirect('/error');
             } else {
-                res.view('pages/detailpost', { detail: data });
+                return res.view('pages/detailpost', { detail: data });
             }
         })
     },
     delete: (req, res) => {
-        Posts.findOne({ id: req.params.id }).populate('author').exec((error, data) => {
-            User.findOne({ id: data.author.id }).exec((error2, data2) => {
-                down = --data2.post;
-                User.update({ id: data.author.id }, { post: down }).fetch().exec((error3, data3) => {
-                    Posts.destroy({ id: req.params.id }).exec((error4, data4) => {
-                        res.redirect('/article');
-                    })
-                })
+        var url;
+        var idauthor;
+        Posts.findOne({ id: req.params.id }).populate('author').then((data) => {
+            idauthor = data.author.id;
+            url = data.picture;
+            return User.findOne({ id: data.author.id });
+        }).then((data2) => {
+            down = --data2.post;
+            return User.update({ id: idauthor }, { post: down }).fetch();
+        }).then((data3) => {
+            return Posts.destroy({ id: req.params.id });
+        }).then((data4) => {
+            const urlParts = url.split("/");
+            const picName = urlParts[urlParts.length - 1];
+            fs.unlinkSync(`assets/images/${picName}`);
+            return res.redirect('/article')
+        }).catch((e) => {
+            return res.json({
+                success: false,
+                message: e
             })
         })
 
     },
     detailsedit: (req, res) => {
         Posts.findOne({ id: req.params.id }).populate('author').exec((error, data) => {
-            if (!data) {
+            if (error) {
+                req.session.flash = {
+                    message: error
+                }
+                return res.redirect('/error');
+            } else if (!data) {
                 req.session.flash = {
                     message: "No post found"
                 }
-                res.redirect('/error');
+                return res.redirect('/error');
             } else {
-                res.view('pages/editpost', { detailsedit: data });
+                return res.view('pages/editpost', { detailsedit: data });
             }
         })
     },
@@ -111,12 +145,16 @@ module.exports = {
             picture: req.body.picture,
             description: req.body.description
         }).fetch().exec((error, data) => {
-            if (data) {
-                res.json({
+            if (error) {
+                return res.json({
+                    message: 'Update error'
+                })
+            } else if (data) {
+                return res.json({
                     message: 'Update success'
                 })
             } else {
-                res.json({
+                return res.json({
                     message: 'Update failed'
                 })
             }
@@ -137,27 +175,24 @@ module.exports = {
         var total;
         var db = Posts.getDatastore().manager;
         var rawMongoCollection = db.collection(Posts.tableName);
-
-        Posts.count().exec((error, data) => {
+        var r = [];
+        Posts.count().then((data) => {
             total = data;
-        });
-        rawMongoCollection.aggregate([{
-                $lookup: {
-                    from: "user",
-                    localField: "author",
-                    foreignField: "_id",
-                    as: "tri"
-                }
-            }, { $unwind: "$tri" },
-            { $match: { $or: [{ title: { $regex: req.query.search.value, $options: 'i' } }, { "tri.name": { $regex: req.query.search.value, $options: 'i' } }] } }, { $sort: b }, { $skip: Number(req.query.start) }, { $limit: Number(req.query.length) }
-        ]).toArray((e, a) => {
-
-            var r = [];
-            a.forEach(e => {
-                r.push([e._id, e.title, e.makeday, e.tri.name, "<a class='btn btn-info' href='/postdetail/" + e._id + "' role='button'>View</a> <a class='btn btn-info' href='/editpost/" + e._id + "' role='button'>Edit</a> <a href='/posts/delete/" + e._id + "' class='btn btn-info' role='button'>Delete</a>"]);
+            return rawMongoCollection.aggregate([{
+                    $lookup: {
+                        from: "user",
+                        localField: "author",
+                        foreignField: "_id",
+                        as: "tri"
+                    }
+                }, { $unwind: "$tri" },
+                { $match: { $or: [{ title: { $regex: req.query.search.value, $options: 'i' } }, { "tri.name": { $regex: req.query.search.value, $options: 'i' } }] } }, { $sort: b }, { $skip: Number(req.query.start) }, { $limit: Number(req.query.length) }
+            ]).toArray();
+        }).then((data2) => {
+            data2.forEach(e => {
+                r.push([e._id, e.title, e.makeday, e.tri.name, `<a class='btn btn-info' href='/postdetail/${e._id}' role='button'>View</a> <a class='btn btn-info' href='/editpost/${e._id}' role='button'>Edit</a> <a href='/posts/delete/${e._id}' onclick="return confirm('Are you sure you want to delete this article?');" class='btn btn-info' role='button'>Delete</a>`]);
             });
-
-            rawMongoCollection.aggregate([{
+            return rawMongoCollection.aggregate([{
                     $lookup: {
                         from: "user",
                         localField: "author",
@@ -166,28 +201,19 @@ module.exports = {
                     }
                 }, { $unwind: "$tri" },
                 { $match: { $or: [{ title: { $regex: req.query.search.value, $options: 'i' } }, { "tri.name": { $regex: req.query.search.value, $options: 'i' } }] } }, { $count: "pass" }
-            ]).toArray((error, data6) => {
-
-                if (data6.length == 0) {
-                    res.json({
-                        "draw": req.query.draw,
-                        "recordsTotal": total,
-                        "recordsFiltered": 0,
-                        "data": r
-                    })
-                } else {
-                    res.json({
-                        "draw": req.query.draw,
-                        "recordsTotal": total,
-                        "recordsFiltered": data6[0].pass,
-                        "data": r
-                    })
-                }
+            ]).toArray();
+        }).then((data3) => {
+            return res.json({
+                "draw": req.query.draw,
+                "recordsTotal": total,
+                "recordsFiltered": data3.length == 0 ? 0 : data3[0].pass,
+                "data": r
             })
-
-
-
-
+        }).catch((e) => {
+            return res.json({
+                success: false,
+                message: e
+            })
         })
 
     }
