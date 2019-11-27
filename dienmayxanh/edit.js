0734d555896f46e72ae9d1a81d875171e3eb3eb8
Le Trong Tri <trichuot99@gmail.com>
2019-11-28 00:21:22 +0700

    wednesdaynight

--- dienmayxanh/edit.js
+++ dienmayxanh/edit.js
@@ -36,505 +36,605 @@ const moveleft = () => {
 data = [{
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "HGY61FIS6NU",
-        "price": "969,08đ"
+        "price": "969,08đ",
+        "inch": "32"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "NSV69EZM7ZV",
-        "price": "779,64đ"
+        "price": "779,64đ",
+        "inch": "32"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "DBN31NHJ4RC",
-        "price": "574,73đ"
+        "price": "574,73đ",
+        "inch": "32"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50'",
         "id": "XEO94YFI6VI",
-        "price": "470,36đ"
+        "price": "470,36đ",
+        "inch": "32"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "FPC79GGF3AS",
-        "price": "577,91đ"
+        "price": "577,91đ",
+        "inch": "32"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "UMB88FCM4OB",
-        "price": "882,17đ"
+        "price": "882,17đ",
+        "inch": "32"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "WTO26CLU3YA",
-        "price": "215,14đ"
+        "price": "215,14đ",
+        "inch": "32"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "SJT85LEA4ZJ",
-        "price": "923,11đ"
+        "price": "923,11đ",
+        "inch": "32"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "QWU00NSW6QF",
-        "price": "680,07đ"
+        "price": "680,07đ",
+        "inch": "32"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "VRO82GRL5GZ",
-        "price": "174,59đ"
+        "price": "174,59đ",
+        "inch": "32"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "ZTX61DBY5KW",
-        "price": "372,18đ"
+        "price": "372,18đ",
+        "inch": "75"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "XKQ34XIJ6MV",
-        "price": "983,90đ"
+        "price": "983,90đ",
+        "inch": "75"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "ACR73TLI6LH",
-        "price": "192,44đ"
+        "price": "192,44đ",
+        "inch": "75"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "QAK41TLB8XV",
-        "price": "123,70đ"
+        "price": "123,70đ",
+        "inch": "75"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "ZTR39UOA0BN",
-        "price": "408,17đ"
+        "price": "408,17đ",
+        "inch": "75"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "IHZ00BLO9RD",
-        "price": "427,43đ"
+        "price": "427,43đ",
+        "inch": "75"
     },
     {
         "title": "SAMSUNG SMART TV 4K 50''",
         "id": "XFQ48BWY3UO",
-        "price": "300,13đ"
+        "price": "300,13đ",
+        "inch": "75"
     },
     {
         "title": "Theodore Beard",
         "id": "JMC63USW2ZH",
-        "price": "696,86đ"
+        "price": "696,86đ",
+        "inch": "75"
     },
     {
         "title": "Elijah Robinson",
         "id": "OOR25VXT8ZL",
-        "price": "691,03đ"
+        "price": "691,03đ",
+        "inch": "75"
     },
     {
         "title": "Joshua Monroe",
         "id": "YVQ31KSR2UF",
-        "price": "656,18đ"
+        "price": "656,18đ",
+        "inch": "75"
     },
     {
         "title": "Chadwick Oconnor",
         "id": "UOQ47DOG2AE",
-        "price": "742,76đ"
+        "price": "742,76đ",
+        "inch": "75"
     },
     {
         "title": "Mufutau Mckinney",
         "id": "GBZ66FHF2JL",
-        "price": "672,07đ"
+        "price": "672,07đ",
+        "inch": "75"
     },
     {
         "title": "Fritz Stone",
         "id": "HOK19WSP9JP",
-        "price": "182,19đ"
+        "price": "182,19đ",
+        "inch": "75"
     },
     {
         "title": "Keefe Hale",
         "id": "CSD20TDX1KO",
-        "price": "872,27đ"
+        "price": "872,27đ",
+        "inch": "65-74"
     },
     {
         "title": "Carson Oneil",
         "id": "OOQ89XFA4HN",
-        "price": "783,58đ"
+        "price": "783,58đ",
+        "inch": "65-74"
     },
     {
         "title": "Neil Brown",
         "id": "TFI96KFX3YD",
-        "price": "573,92đ"
+        "price": "573,92đ",
+        "inch": "65-74"
     },
     {
         "title": "Colton Mathews",
         "id": "PJM38TNW3XG",
-        "price": "222,86đ"
+        "price": "222,86đ",
+        "inch": "65-74"
     },
     {
         "title": "Palmer Spears",
         "id": "GUV79XTW8AB",
-        "price": "811,70đ"
+        "price": "811,70đ",
+        "inch": "65-74"
     },
     {
         "title": "Reese Yang",
         "id": "PWZ78IBD9OX",
-        "price": "335,04đ"
+        "price": "335,04đ",
+        "inch": "65-74"
     },
     {
         "title": "Jackson Sawyer",
         "id": "QMQ88EMV2LR",
-        "price": "230,02đ"
+        "price": "230,02đ",
+        "inch": "65-74"
     },
     {
         "title": "Kuame Lara",
         "id": "LSQ37TQR8FG",
-        "price": "301,46đ"
+        "price": "301,46đ",
+        "inch": "65-74"
     },
     {
         "title": "Stuart Ware",
         "id": "KQL96HVX0NE",
-        "price": "256,57đ"
+        "price": "256,57đ",
+        "inch": "65-74"
     },
     {
         "title": "Jason Day",
         "id": "MHN32AFZ4JI",
-        "price": "753,64đ"
+        "price": "753,64đ",
+        "inch": "65-74"
     },
     {
         "title": "Harlan Rivas",
         "id": "XRH10DCV2GV",
-        "price": "935,03đ"
+        "price": "935,03đ",
+        "inch": "65-74"
     },
     {
         "title": "Russell Anderson",
         "id": "YEP76TXS8GZ",
-        "price": "720,37đ"
+        "price": "720,37đ",
+        "inch": "65-74"
     },
     {
         "title": "Kenneth Dawson",
         "id": "OWK84KRQ3NN",
-        "price": "420,80đ"
+        "price": "420,80đ",
+        "inch": "65-74"
     },
     {
         "title": "Calvin Singleton",
         "id": "YYO97KHM9FQ",
-        "price": "484,09đ"
+        "price": "484,09đ",
+        "inch": "55-64"
     },
     {
         "title": "Coby Rocha",
         "id": "VNB44TOQ1GW",
-        "price": "788,00đ"
+        "price": "788,00đ",
+        "inch": "55-64"
     },
     {
         "title": "Preston Walters",
         "id": "UJE57PME7CE",
-        "price": "605,98đ"
+        "price": "605,98đ",
+        "inch": "55-64"
     },
     {
         "title": "Caleb Dalton",
         "id": "FHD87FJO8NP",
-        "price": "389,65đ"
+        "price": "389,65đ",
+        "inch": "55-64"
     },
     {
         "title": "Hyatt May",
         "id": "TKX48LKS1YA",
-        "price": "677,22đ"
+        "price": "677,22đ",
+        "inch": "55-64"
     },
     {
         "title": "Dorian Harrell",
         "id": "CRQ22QLH8HC",
-        "price": "975,75đ"
+        "price": "975,75đ",
+        "inch": "55-64"
     },
     {
         "title": "Silas Hamilton",
         "id": "ZXO10CYF3WV",
-        "price": "398,27đ"
+        "price": "398,27đ",
+        "inch": "55-64"
     },
     {
         "title": "Dylan Melendez",
         "id": "XPN50BON3JK",
-        "price": "647,72đ"
+        "price": "647,72đ",
+        "inch": "55-64"
     },
     {
         "title": "Barry Hampton",
         "id": "SVN22ESX8ZA",
-        "price": "539,80đ"
+        "price": "539,80đ",
+        "inch": "55-64"
     },
     {
         "title": "Jacob Park",
         "id": "MEM14WIF4QZ",
-        "price": "954,51đ"
+        "price": "954,51đ",
+        "inch": "55-64"
     },
     {
         "title": "Cairo Bowman",
         "id": "HFP65UML4OD",
-        "price": "274,10đ"
+        "price": "274,10đ",
+        "inch": "55-64"
     },
     {
         "title": "Brenden Weiss",
         "id": "FUV16VVY5JC",
-        "price": "646,91đ"
+        "price": "646,91đ",
+        "inch": "55-64"
     },
     {
         "title": "Yuli Reed",
         "id": "ZIG04RBD8RT",
-        "price": "227,47đ"
+        "price": "227,47đ",
+        "inch": "55-64"
     },
     {
         "title": "Ira Cobb",
         "id": "ZQS35IXU9OX",
-        "price": "960,54đ"
+        "price": "960,54đ",
+        "inch": "55-64"
     },
     {
         "title": "Peter Velasquez",
         "id": "XXR47FSK6RQ",
-        "price": "363,64đ"
+        "price": "363,64đ",
+        "inch": "55-64"
     },
     {
         "title": "Burke Fox",
         "id": "HLW15XJF4VO",
-        "price": "222,50đ"
+        "price": "222,50đ",
+        "inch": "55-64"
     },
     {
         "title": "Leonard Klein",
         "id": "EPL90HMV7VR",
-        "price": "804,11đ"
+        "price": "804,11đ",
+        "inch": "55-64"
     },
     {
         "title": "Orlando Holt",
         "id": "IMG16WEW3JN",
-        "price": "519,59đ"
+        "price": "519,59đ",
+        "inch": "55-64"
     },
     {
         "title": "Keith Nolan",
         "id": "HMR12XAB0HZ",
-        "price": "158,89đ"
+        "price": "158,89đ",
+        "inch": "55-64"
     },
     {
         "title": "Ivor Parrish",
         "id": "OPL63JZK8GV",
-        "price": "871,34đ"
+        "price": "871,34đ",
+        "inch": "32-43"
     },
     {
         "title": "Jonas Brady",
         "id": "FSI73XHE1BI",
-        "price": "325,07đ"
+        "price": "325,07đ",
+        "inch": "32-43"
     },
     {
         "title": "Samson Leonard",
         "id": "RQG56OLG9NW",
-        "price": "604,90đ"
+        "price": "604,90đ",
+        "inch": "32-43"
     },
     {
         "title": "Brady Hunt",
         "id": "CUB05YUO2BX",
-        "price": "182,32đ"
+        "price": "182,32đ",
+        "inch": "32-43"
     },
     {
         "title": "Tucker Sampson",
         "id": "THA29LWY3LQ",
-        "price": "781,08đ"
+        "price": "781,08đ",
+        "inch": "32-43"
     },
     {
         "title": "Walker Morgan",
         "id": "QHB00EKQ0OV",
-        "price": "724,13đ"
+        "price": "724,13đ",
+        "inch": "32-43"
     },
     {
         "title": "Richard Clark",
         "id": "WXI33OUX8FS",
-        "price": "598,24đ"
+        "price": "598,24đ",
+        "inch": "32-43"
     },
     {
         "title": "Abbot Mills",
         "id": "FPQ02KWY8SH",
-        "price": "913,75đ"
+        "price": "913,75đ",
+        "inch": "55-64"
     },
     {
         "title": "Reuben Stanley",
         "id": "XSA80SHM6XR",
-        "price": "436,39đ"
+        "price": "436,39đ",
+        "inch": "55-64"
     },
     {
         "title": "Edan Wolf",
         "id": "OMB29SII6WV",
-        "price": "487,90đ"
+        "price": "487,90đ",
+        "inch": "55-64"
     },
     {
         "title": "Omar Coffey",
         "id": "XOR38QAJ5SY",
-        "price": "598,01đ"
+        "price": "598,01đ",
+        "inch": "55-64"
     },
     {
         "title": "Darius Ortega",
         "id": "FJE43MAJ0LP",
-        "price": "456,60đ"
+        "price": "456,60đ",
+        "inch": "55-64"
     },
     {
         "title": "Devin Peterson",
         "id": "EWJ06OTH4IA",
-        "price": "182,48đ"
+        "price": "182,48đ",
+        "inch": "55-64"
     },
     {
         "title": "Salvador Diaz",
         "id": "BAF68EKO8QN",
-        "price": "738,77đ"
+        "price": "738,77đ",
+        "inch": "55-64"
     },
     {
         "title": "Myles Gillespie",
         "id": "TNG83ZFM8KA",
-        "price": "279,52đ"
+        "price": "279,52đ",
+        "inch": "55-64"
     },
     {
         "title": "Buckminster Estes",
         "id": "SCX93YHE0GM",
-        "price": "209,05đ"
+        "price": "209,05đ",
+        "inch": "55-64"
     },
     {
         "title": "Emerson Hanson",
         "id": "MTE09CWM2JG",
-        "price": "126,77đ"
+        "price": "126,77đ",
+        "inch": "55-64"
     },
     {
         "title": "Porter Donaldson",
         "id": "HCJ15LCG6FI",
-        "price": "428,73đ"
+        "price": "428,73đ",
+        "inch": "55-64"
     },
     {
         "title": "Branden Haney",
         "id": "UMD55PSW0SE",
-        "price": "646,59đ"
+        "price": "646,59đ",
+        "inch": "55-64"
     },
     {
         "title": "Adrian Gray",
         "id": "KWM20HKN3KO",
-        "price": "798,12đ"
+        "price": "798,12đ",
+        "inch": "55-64"
     },
     {
         "title": "Hilel Sweeney",
         "id": "YSJ92VIX3HH",
-        "price": "355,63đ"
+        "price": "355,63đ",
+        "inch": "55-64"
     },
     {
         "title": "Wyatt Rodgers",
         "id": "EFR85PTS0XB",
-        "price": "551,50đ"
+        "price": "551,50đ",
+        "inch": "55-64"
     },
     {
         "title": "Caesar Dudley",
         "id": "MZJ03IKB0RZ",
-        "price": "110,28đ"
+        "price": "110,28đ",
+        "inch": "55-64"
     },
     {
         "title": "Chaim Rios",
         "id": "QVJ12STG7KL",
-        "price": "833,70đ"
+        "price": "833,70đ",
+        "inch": "55-64"
     },
     {
         "title": "Chancellor Price",
         "id": "UQP46GLB5LY",
-        "price": "628,48đ"
+        "price": "628,48đ",
+        "inch": "55-64"
     },
     {
         "title": "Lucas Dawson",
         "id": "UWV96PGF5RN",
-        "price": "664,30đ"
+        "price": "664,30đ",
+        "inch": "55-64"
     },
     {
         "title": "Thane Daniels",
         "id": "QWR71HUO1ZS",
-        "price": "306,00đ"
+        "price": "306,00đ",
+        "inch": "55-64"
     },
     {
         "title": "Drake Alvarado",
         "id": "FNV23LXM2BL",
-        "price": "652,55đ"
+        "price": "652,55đ",
+        "inch": "55-64"
     },
     {
         "title": "Malachi Noble",
         "id": "DLJ85AMI6YV",
-        "price": "962,10đ"
+        "price": "962,10đ",
+        "inch": "55-64"
     },
     {
         "title": "James Greene",
         "id": "PRR52ILS2LJ",
-        "price": "733,09đ"
+        "price": "733,09đ",
+        "inch": "55-64"
     },
     {
         "title": "Harding Logan",
         "id": "KCG88SVE9TI",
-        "price": "674,45đ"
+        "price": "674,45đ",
+        "inch": "55-64"
     },
     {
         "title": "Marvin Bentley",
         "id": "WFF98MVA8JC",
-        "price": "431,31đ"
+        "price": "431,31đ",
+        "inch": "55-64"
     },
     {
         "title": "Jerry Bell",
         "id": "GBB39MNG5DM",
-        "price": "122,70đ"
+        "price": "122,70đ",
+        "inch": "55-64"
     },
     {
         "title": "Evan Wolfe",
         "id": "XHU13UTH7GA",
-        "price": "676,59đ"
+        "price": "676,59đ",
+        "inch": "44-54"
     },
     {
         "title": "Kibo Potter",
         "id": "CRV78OFQ9ZX",
-        "price": "552,69đ"
+        "price": "552,69đ",
+        "inch": "44-54"
     },
     {
         "title": "Malachi Lyons",
         "id": "MAC11NJH0QF",
-        "price": "904,40đ"
+        "price": "904,40đ",
+        "inch": "44-54"
     },
     {
         "title": "Basil Harrison",
         "id": "ZPM67WDP8ZC",
-        "price": "463,08đ"
+        "price": "463,08đ",
+        "inch": "44-54"
     },
     {
         "title": "Theodore Phillips",
         "id": "TMF28FGN5MD",
-        "price": "147,36đ"
+        "price": "147,36đ",
+        "inch": "44-54"
     },
     {
         "title": "Clinton Oneill",
         "id": "BXU38KPY9BY",
-        "price": "765,27đ"
+        "price": "765,27đ",
+        "inch": "44-54"
     },
     {
         "title": "Beck Booth",
         "id": "LNC92ZMN9AC",
-        "price": "877,85đ"
+        "price": "877,85đ",
+        "inch": "44-54",
     },
     {
         "title": "Hamilton Santos",
         "id": "IJO66PGG4GN",
-        "price": "901,48đ"
+        "price": "901,48đ",
+        "inch": "55-64"
     },
     {
         "title": "Drew Thornton",
         "id": "OVU04GFY2UN",
-        "price": "956,80đ"
+        "price": "956,80đ",
+        "inch": "55-64"
     },
     {
         "title": "Oliver Cannon",
         "id": "XYU18BUY6ZK",
-        "price": "179,36đ"
+        "price": "179,36đ",
+        "inch": "55-64"
     },
     {
         "title": "Jakeem Carney",
         "id": "ZBV24SIW9SS",
-        "price": "467,81đ"
+        "price": "467,81đ",
+        "inch": "55-64"
     },
     {
         "title": "Julian Whitfield",
         "id": "WAP96QMM5JR",
-        "price": "721,91đ"
+        "price": "721,91đ",
+        "inch": "55-64"
     }
 ];
-for (var i = 0; i < 12; i++) {
+for (var i = 0; i < data.length; i++) {
     document.getElementById("artdetail").insertAdjacentHTML('beforeend', `
     <div class="card">
     <div class="lab">
@@ -546,6 +646,7 @@ for (var i = 0; i < 12; i++) {
         <img src="images/online.png">
         <h4 style="margin-bottom: 4px;">${data[i].title}</h4>
         <p style="margin-bottom: 4px;">${data[i].id}</p>
+        <p class="incher">${data[i].inch}</p>
         <h4 style="margin-bottom: 4px;" class="price">${data[i].price}</h4>
         <div style="margin-bottom:4px" class="sub">
             <div>
@@ -587,4 +688,81 @@ for (var i = 0; i < 12; i++) {
         btnsup.parentNode.removeChild(btnsup);
     }
 
+}
+window.onscroll = function() { myFunction() };
+
+// Get the navbar
+var order = document.getElementById("order");
+
+// Get the offset position of the navbar
+var fixed = order.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= fixed) {
+        order.classList.add("fix")
+    } else {
+        order.classList.remove("fix");
+    }
+}
+var first = 0;
+const filter = document.getElementsByClassName("boxfilter");
+for (var i = 0; i < filter.length; i++) {
+    filter[i].addEventListener('click', (e) => {
+        first++;
+        if (e.target.checked) {
+            if (first == 1) {
+                document.getElementById("artdetail").innerHTML = " ";
+            }
+            for (var m = 0; m < data.length; m++) {
+                if (data[m].inch == e.target.value) {
+                    document.getElementById("artdetail").insertAdjacentHTML('beforeend', `
+    <div class="card">
+    <div class="lab">
+        <label class="sel">#1 Bán Chạy</label>
+        <label class="pay">Trả góp 0%</label>
+    </div>
+    <img class="pict" src="images/tv.png" alt="Avatar" style="width:100%">
+    <div class="content">
+        <img src="images/online.png">
+        <h4 style="margin-bottom: 4px;">${data[m].title}</h4>
+        <p style="margin-bottom: 4px;">${data[m].id}</p>
+        <p class="incher">${data[m].inch}</p>
+        <h4 style="margin-bottom: 4px;" class="price">${data[m].price}</h4>
+        <div style="margin-bottom:4px" class="sub">
+            <div>
+                <h5 class="deduct">13.500.000đ</h5>
+            </div>
+            <div>
+                <span class="per">(-12%)</span>
+            </div>
+        </div>
+        <p style="margin-bottom: 4px;">
+            Quà<span class="gift"> 300.000đ</span>
+        </p>
+        <div class="comment">
+            <span class="rate">4.3/5</span><i id="star" class="fas fa-star"></i> 300 đánh giá
+        </div>
+
+    </div>
+</div>
+    `);
+                }
+            }
+        } else {
+            const real = document.getElementsByClassName("incher");
+            const par = document.getElementById("artdetail");
+            console.log(real.length);
+            var del = [];
+            for (var k = 0; k < real.length; k++) {
+                if (real[k].innerText == e.target.value) {
+                    const chil = document.getElementsByClassName("card")[k];
+                    del.push(chil);
+                }
+            }
+            for (var l = 0; l < del.length; l++) {
+                par.removeChild(del[l]);
+            }
+        }
+    })
 }
